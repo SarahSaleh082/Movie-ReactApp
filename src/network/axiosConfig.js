16b1a9fce56275461685af3466595f106058da6c
sarahsaleh082 <sarahsaleh082@gmail.com>
2022-01-26 04:39:57 -0800

    add get API

---
+++ src/network/axiosConfig.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+export const axiosInstace = axios.create({
+  baseURL: "https://api.themoviedb.org/3/discover/movie",
+  params: {
+    api_key: '60092b7ad34698fddfac65cb5743e4e1'
+  }
+});
+
+
+
+// // Add a request interceptor
+// axiosInstace.interceptors.request.use(
+//   function (config) {
+//     // Do something before request is sent
+//     console.log("Interceptor", config);
+//     // config.headers = {
+//     //   ...config.headers,
+//     //   Authorization: localStorage.getItem("token"),
+//     // };
+//     if (localStorage.getItem("token")) {
+//       config.headers["Authorization"] = localStorage.getItem("token");
+//     }
+//     config.params = {
+//       ...config.params,
+//       "Api-key": "daskjdalsd5da6sd",
+//     };
+//     return config;
+//   },
+//   function (error) {
+//     // Do something with request error
+//     return Promise.reject(error);
+//   }
+// );
+
+// // Add a response interceptor
+// axiosInstace.interceptors.response.use(
+//   function (response) {
+//     // Any status code that lie within the range of 2xx cause this function to trigger
+//     // Do something with response data
+//     return response;
+//   },
+//   function (error) {
+//     // Any status codes that falls outside the range of 2xx cause this function to trigger
+//     // Do something with response error
+//     return Promise.reject(error);
+//   }
+// );
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {axiosInstace} from '../network/axiosConfig'
 
 export default function Movies() {
+    const [movieList, setMovieList] = useState([]);
+    useEffect(()=>{
+        axiosInstace.get('/')
+        .then((res)=>{
+            setMovieList(res.data.results);
+        })
+        .catch(()=>{})
+    }, [])
   return <div></div>;
 }
